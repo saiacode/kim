07fbb5ba2b8abb22feb165435b4c65ca58936b2d
Alexander Ditzend <alexander.ditzend@mitrol.net>
2020-10-15 19:19:23 -0300

    classes

---
+++ Layer.js
@@ -0,0 +1,29 @@
+class Layer {
+  constructor() {
+    this.sides = SIDES;
+    this.numShapes = this.sides;
+    this.angle = 360 / this.numShapes;
+    this.stepsOut = 8;
+    this.singleStep = CRYSTAL_SIZE / 2 / this.stepsOut;
+    this.thinStroke = 1;
+    this.thickStroke = 3;
+  }
+}
+
+class Circles extends Layer {
+  constructor() {
+    super();
+    this.shapeSize = (CRYSTAL_SIZE / 2) * 0.93;
+    this.position = CRYSTAL_SIZE / 2 - this.shapeSize / 2;
+  }
+    render() {
+      noFill()
+    push();
+    translate(width / 2, height / 2);
+    for (let i = 0; i <= this.numShapes; i++) {
+      ellipse(this.position, 0, this.shapeSize, this.shapeSize);
+      rotate(this.angle);
+    }
+    pop();
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,8 +1,67 @@
-function setup() {
-    console.log('hi')
 
+
+const CRYSTAL_SIZE = 500
+const SIDES = 6
+
+
+function setup() {
+createCanvas(530,530, SVG)
+    noLoop()
+    angleMode(DEGREES)
+    rectMode(CENTER)
 }
 
 function draw() {
-    
+    const layer = new Circles()
+    layer.render()
+//     testLines()
+//    outlineShape()
+//     simpleLines()
+//     circles()
+}
+
+function simpleLines() {
+    const stepsOut = 8
+    const numSteps = floor(stepsOut * 1.25)
+    const step = (CRYSTAL_SIZE / 2) / numSteps
+    const start = floor(random(0, numSteps))
+    const stop = floor(random(start, numSteps + 1))
+
+
+         noFill();
+         push();
+         translate(width / 2, height / 2);
+         ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
+         for (let i = 0; i < SIDES; i++) {
+           line(start * step, 0, stop * step, 0);
+           rotate(360 / SIDES);
+         }
+         pop();
+}
+
+
+function outlineShape() {
+    stroke('black')
+    strokeWeight(5)
+    push()
+    translate(width / 2, height / 2)
+    // ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)
+    // myline()
+    hexagon(0,0, CRYSTAL_SIZE/2)
+    pop()
+}
+
+
+
+
+function testLines() {
+     noFill();
+     push();
+     translate(width / 2, height / 2);
+     ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
+     for (let i = 0; i < SIDES; i++) {
+       line(0, 0, CRYSTAL_SIZE / 2, 0);
+       rotate(360 / SIDES);
+     }
+     pop();
 }
